461155fc5937cc8804f4ba27def2852ab95e4aa6
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-01-07 12:08:00 +0100

    Added allowed host open

--- clairvoyance/settings.py
+++ clairvoyance/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['193.3.238.103']
 
 
 # Application definition
