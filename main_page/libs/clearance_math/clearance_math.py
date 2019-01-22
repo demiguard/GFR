d0edc4589702e32e59223181eed3a8b052173e8a
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2019-01-22 11:56:32 +0100

    Added test of GFR for CR-51, and multiple points

--- main_page/libs/clearance_math/clearance_math.py
+++ main_page/libs/clearance_math/clearance_math.py
@@ -2,6 +2,7 @@ import numpy
 import matplotlib.pyplot as plt
 import pandas
 import datetime
+from scipy.stats import linregress
 
 class table_info():
   def __init__(self, study_time, cnt, pos, rack, date, run_id):
@@ -33,7 +34,7 @@ def surface_area(height, weight, method = "Du Bois"):
   else:
     return -1
 
-def calc_clearance(inj_time, sample_time, tec99_cnt, BSA, dosis, method = "EPV")
+def calc_clearance(inj_time, sample_time, tec99_cnt, BSA, dosis, method = "EPV"):
   """
   Calculate the Clearence, using the functions from clearance_function.php
 
@@ -41,7 +42,7 @@ def calc_clearance(inj_time, sample_time, tec99_cnt, BSA, dosis, method = "EPV")
   Argument:
     inj_time: A Date from datetime containing information when injection happened 
     sample_time: a list of dates from datetime containing formation when the bloodsample was taken
-    tec99_cnt: A list of int containing the counts from
+    tec99_cnt: A list of int containing the counts from the samples
     BSA: a float, representing body surface area, Use Surface_area
     dosis: A float with calculation of the dosis size, Use dosis
 
@@ -52,10 +53,10 @@ def calc_clearance(inj_time, sample_time, tec99_cnt, BSA, dosis, method = "EPV")
     clearence, clearence-normalized
   """
   
-  delta_times = [] #timedelta list from timedate
-  for time in sample_time:
-    #Compute how many minutes between injection and 
-    delta_times.append(time-inj_time).second / 60
+  delta_times = [(time - inj_time).seconds / 60 for time in sample_time] #timedelta list from timedate
+  # for time in sample_time:
+  #   #Compute how many minutes between injection and 
+  #   delta_times.append((time-inj_time).seconds / 60)
 
   if method == "EPV":
     #In this method deltatimes and tec99_cnt lenght is equal to one
@@ -65,41 +66,49 @@ def calc_clearance(inj_time, sample_time, tec99_cnt, BSA, dosis, method = "EPV")
     magic_number_3 = 1.88
     magic_number_4 = 928
 
-    clearence = 
-      (magic_number_1 * delta_times[0] - magic_number_2) * 
-      numpy.log(tec99_cnt[0] * BSA / dosis )
-      + magic_number_3 * delta_times[0] - magic_number_4
+    clearence_normalized = (magic_number_1 * delta_times[0] - magic_number_2) * numpy.log(tec99_cnt[0] * BSA / dosis ) + magic_number_3 * delta_times[0] - magic_number_4
     #
     magic_number_5 = 1.73
-    clearence_normalized = clearence * BSA / magic_number_5 
+    clearence = clearence_normalized * BSA / magic_number_5 
   elif method == "EPB":
     #
     #Magical Numbers
     magic_number_1 = 5867
     magic_number_2 = 1.1792
     
-    ECV = magic_number_1 * BSA ** magic_number_2
+    ECV = magic_number_1 * (BSA ** magic_number_2)
 
     magic_number_3 = 1.01
     magic_number_4 = -0.00011
     magic_number_5 = 0.538
-    magic_number_6 = -0.178 
+    magic_number_6 = -0.0178 
   
-    g =
-      magic_number_3 * numpy.exp(magic_number_4 * delta_times[0]) 
-      + magic_number_5 * numpy.exp(magic_number_6 * delta_times[0])
-    
-    clearence = (numpy.log(tec99_cnt[0]* ECV / dosis)*ECV)/(delta_times[0]*g)
+    g = magic_number_3 * numpy.exp(magic_number_4 * delta_times[0]) + magic_number_5 * numpy.exp(magic_number_6 * delta_times[0])
+    clearence = ( -numpy.log(tec99_cnt[0]* ECV / dosis) * ECV) / (delta_times[0] * g)
     
     magic_number_7 = 1.73
 
-    clearence_normalized =  clearence * magic_number_7 / BSA
-  elif method == "Multi-4"
-    pass
+    clearence_normalized =  (clearence * magic_number_7) / BSA
+  elif method == "Multi-4" :
+
+    log_tec99_cnt = [numpy.log(x) for x in tec99_cnt]
+
+    slope, intercept, _, _, _ =  linregress(delta_times , log_tec99_cnt)
+  
+    clearence_reg = (dosis * (-slope)) / numpy.exp(intercept) 
+
+    magic_number_1 = 0.990778
+    magic_number_2 = 0.001218
+
+    clearence = magic_number_1 * clearence_reg - magic_number_2 * clearence_reg**2
+    
+    magic_number_3 = 1.73
+
+    clearence_normalized = clearence * magic_number_3 / BSA
+
   else:
     print("this shouldnt happen")
     return -1, -1
-
   return clearence, clearence_normalized
 
 def dosis(inj, fac, stc):
@@ -111,7 +120,7 @@ def dosis(inj, fac, stc):
     stc: standard count
 
   """
- return inj*fac*stcx
+  return int(inj*fac*stc)
 
 def cpr_birth(cpr):
   pass
@@ -161,12 +170,21 @@ def import_csv(csv_path, machine ='', method='Cr-51 Counts'):
 
 
 
-def generate_plot(data_points1, data_points2, rigs_nr, hosp_dir='RH', imageHeight = 10.8, imageWidth = 19.2):
+def generate_plot(
+  data_points1,
+  data_points2,
+  rigs_nr,
+  hosp_dir='RH',
+  imageHeight = 10.8,
+  imageWidth = 19.2,
+  save_fig = True,
+  show_fig = False
+  ):
   """
   Generate GFR plot
 
   Args:
-    data_points1: Data points for first graph
+    data_points1: [[npoints],[npoints]] - numpy.array Data points for first graph 
     data_points2: Data points for second graph
 
 
@@ -178,25 +196,25 @@ def generate_plot(data_points1, data_points2, rigs_nr, hosp_dir='RH', imageHeigh
   # TODO: These values define changed
   save_dir = 'main_page/static/main_page/images/{0}'.format(hosp_dir)
 
-  x =           [0, 40, 100]
+  x =           [0, 40, 110]
   zeros =       [0, 0, 0]
   darkred_y =   [30, 30, 10]
   light_red_y = [50, 50, 30]
   yellow_y =    [75, 75, 35]
-  lightgrey_y = [130, 130, 90]
-  grey_y =      [130, 130, 130]
+  lightgrey_y = [160, 160, 160]
+  #grey_y =      [130, 130, 130]
 
   fig, ax = plt.subplots(1, 2)
-
+  
 
   # Generate backgrounds for second graph
-  ax[1].set_xlim(0, 100)
-  ax[1].set_ylim(0, 130)
+  ax[1].set_xlim(0, 110)
+  ax[1].set_ylim(0, 160)
   ax[1].fill_between(x, zeros, darkred_y, facecolor='#F96564', label='Svært nedsat')
   ax[1].fill_between(x, darkred_y, light_red_y, facecolor='#FBA0A0', label='Middelsvært nedsat')
   ax[1].fill_between(x, light_red_y, yellow_y, facecolor='#FFA71A', label='Moderat nedsat')
   ax[1].fill_between(x, yellow_y, lightgrey_y, facecolor='#EFEFEF', label='Normal')
-  ax[1].fill_between(x, lightgrey_y, grey_y, facecolor='#BEBEBE')
+  #ax[1].fill_between(x, lightgrey_y, grey_y, facecolor='#BEBEBE')
   
   titlesize = 8
   labelsize = 18
@@ -212,10 +230,16 @@ def generate_plot(data_points1, data_points2, rigs_nr, hosp_dir='RH', imageHeigh
   ax[1].set_ylabel('GFR (ml/min pr. 1.73m²)')
   ax[1].grid(color='black')
 
+  ax[0].scatter(data_points1[0,:], data_points1[1,:])
+  ax[1].scatter(data_points2[0,:], data_points2[1,:])
+    
   fig.set_figheight(imageHeight)
   fig.set_figwidth(imageWidth)
   plt.legend()
   image_path = "{0}/{1}.png".format(save_dir,rigs_nr)
-  plt.savefig(image_path)
-  
+  if save_fig : 
+    plt.savefig(image_path)
+  if show_fig :
+    plt.show()
+
   return image_path
