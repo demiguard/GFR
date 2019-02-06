386a7f52df32c166fd4b6380c0c4b253efa606c5
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-02-06 12:52:57 +0100

    added kylle ip

--- clairvoyance/settings.py
+++ clairvoyance/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['193.3.238.103','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['193.3.238.103', '172.16.78.176','127.0.0.1', 'localhost']
 
 
 # Application definition
