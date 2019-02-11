d912daa757b13094a7cab23312784d40ecd179a1
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-02-11 11:08:38 +0100

    Added login/logout feature

--- main_page/backends.py
+++ main_page/backends.py
@@ -4,8 +4,20 @@ from .models import User
 
 # Simple mysql database users
 class SimpleBackend:
-  def authenticate(self, request, username=None, password=None):
-    pass
+  def authenticate(self, request, username=None, password=None, hosp=None):
+    if username and password and hosp:
+      try:
+        user = User.objects.get(username=username, hospital=hosp)
+
+        if check_password(password, user.password):
+          return user
+      except User.DoesNotExist:
+        pass
+
+    return None
 
   def get_user(self, username):
-    pass
+    try:
+      return User.objects.get(pk=username)
+    except User.DoesNotExist:
+      return None
