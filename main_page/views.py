b2413a19ccb31bd9ef6eb03064033e3df933c570
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-02-20 14:59:23 +0100

    Added search page

--- main_page/views.py
+++ main_page/views.py
@@ -246,6 +246,28 @@ def fetch_study(request):
   # Specify page template
   template = loader.get_template('main_page/fetch_study.html')
 
+  # Construct new query file
+  history_dir = './hist_tmp/'
+  search_query = 'search_query'
+  base_search_file = "{0}base_{1}.dcm".format(history_dir, search_query)
+
+  # Find next number for query file
+  search_files = glob.glob('{0}{1}*.dcm'.format(history_dir, search_query))
+  
+  curr_num = len(search_files)
+  curr_search_file = '{0}{1}{2}.dcm'.format(history_dir, search_query, curr_num)
+  
+  curr_rsp_dir = '{0}rsp{1}/'.format(history_dir, curr_num)
+  os.mkdir(curr_rsp_dir)
+
+  # Make a new copy to construct the current query
+  shutil.copyfile(base_search_file, curr_search_file)
+
+  sf = pydicom.dcmread(curr_search_file)
+
+  # Set auto fill parameters
+  auto_fill_params = { }
+
   if 'Søg' in request.GET:
     # Extract search parameters
     s_name = request.GET['name']
@@ -254,25 +276,19 @@ def fetch_study(request):
     s_date_from = request.GET['Dato_start']
     s_date_to = request.GET['Dato_finish']
 
-    # Construct query
-    history_dir = './hist_tmp/'
-    search_query = 'search_query'
-    base_search_file = "{0}base_{1}.dcm".format(history_dir, search_query)
+    # Formatting...
+    s_date_from = s_date_from.replace('-','')
+    s_date_to = s_date_to.replace('-','')
 
-    # Find next number for query file
-    search_files = glob.glob('{0}{1}*.dcm'.format(history_dir, search_query))
-    
-    curr_num = len(search_files)
-    curr_search_file = '{0}{1}{2}.dcm'.format(history_dir, search_query, curr_num)
-    
-    curr_rsp_dir = '{0}rsp{1}/'.format(history_dir, curr_num)
-    os.mkdir(curr_rsp_dir)
+    auto_fill_params = {
+      'name': s_name,
+      'cpr': s_cpr,
+      'Rigs': s_rigs,
+      'Dato_start': s_date_from[:4] + '-' + s_date_from[4:6] + '-' + s_date_from[6:],
+      'Dato_finish': s_date_to[:4] + '-' + s_date_to[4:6] + '-' + s_date_to[6:],
+    }
 
-    # Make a new copy to construct the current query
-    shutil.copyfile(base_search_file, curr_search_file)
-
-    sf = pydicom.dcmread(curr_search_file)
-
+    # Search by name
     if s_name:
       s_name = s_name.strip()
       name_arr = s_name.split(' ')
@@ -298,88 +314,105 @@ def fetch_study(request):
 
       sf.PatientName = name_str
 
-    # if s_cpr:
+    # Search by cpr nr.
+    if s_cpr:
+      s_cpr = s_cpr.replace('-', '')
 
+      sf.PatientID = s_cpr
+      print(sf.PatientID)
 
-    # if s_rigs:
-
+    # Search by rigs number
+    if s_rigs:
+      sf.AccessionNumber = s_rigs
     
-    # if s_date_from:
-
-
-    # if s_date_to:
-
-
-    sf.save_as(curr_search_file)
-
-    search_query = [
-      "findscu",
-      "-S",
-      "-aet",
-      "RH_EDTA",
-      "-aec",
-      "TEST_DCM4CHEE",
-      "127.0.0.1",
-      "11112",
-      curr_search_file,
-      '-X',
-      '-od',
-      curr_rsp_dir
-    ]
+    # Search by date range
+    s_date_range = ''
     
-    # TODO: Add error handling
-    out = ris.execute_query(search_query)
+    if s_date_from:
+      s_date_range += s_date_from + '-'
 
-    # Extract data from responses
-    rsps = []
-
-    for rsp_path in glob.glob('{0}*.dcm'.format(curr_rsp_dir)):
-      # TODO: Change this to use get_examination from ris_query_wrapper to make christoffer happy, yet still try to keep the querie files short
-      # rsp_name = rsp_path.split('.')[0]
-      # curr_exam = ris.get_examination(curr_rsp_dir, rsp_name)
-
-      ds = pydicom.dcmread(rsp_path)
-      rsp_info = (
-        ds.PatientID,
-        ds.PatientName,
-        ds.StudyDate,
-        ds.AccessionNumber,
-      )
-
-      # rsp_info = (
-      #   curr_exam.info['name'],
-      #   curr_exam.info['cpr'],
-      #   curr_exam.info['date'],
-      #   curr_exam.info['ris_nr'],
-      # )
-
-      rsps.append(rsp_info)
-
-    # Remove the search query file
-    os.remove(curr_search_file)
-    shutil.rmtree(curr_rsp_dir)
-
+    if s_date_to:
+      s_date_range += s_date_to
+    
+    sf.StudyDate = s_date_range
   else:
-    # Default case: display the patients from today
-    # search_file = "./hist_tmp/default_search_query.dcm"
-    
-    # default_query = [
-    #   "FINDSCU",
-    #   "-S",
-    #   "-aet",
-    #   "RH_EDTA",
-    #   "-aec",
-    #   "TEST_DCM4CHEE",
-    #   "127.0.0.1",
-    #   "11112",
-    #   search_file
-    # ]
-    rsps = []
+    # Default case: display the patients from the last week
+    now = datetime.datetime.now()
+    now_str = now.strftime('%Y%m%d')
 
+    week_delta = datetime.timedelta(days=7)
+    week_datetime = now - week_delta
+    last_week_str = week_datetime.strftime('%Y%m%d')
+
+    sf.StudyDate = '{0}-{1}'.format(last_week_str, now_str)    
+
+    # Set the dates in the auto fill
+    auto_fill_params = {
+      'Dato_start': last_week_str[:4] + '-' + last_week_str[4:6] + '-' + last_week_str[6:],
+      'Dato_finish': now_str[:4] + '-' + now_str[4:6] + '-' + now_str[6:],
+    }
+
+  # Save and execute the current search query file
+  sf.save_as(curr_search_file)
+
+  search_query = [
+    "findscu",
+    "-S",
+    "-aet",
+    "RH_EDTA",
+    "-aec",
+    "TEST_DCM4CHEE",
+    "127.0.0.1",
+    "11112",
+    curr_search_file,
+    '-X',
+    '-od',
+    curr_rsp_dir
+  ]
+
+  # TODO: Add error handling
+  out = ris.execute_query(search_query)
+
+  # Extract data from responses
+  rsps = []
+
+  for rsp_path in glob.glob('{0}*.dcm'.format(curr_rsp_dir)):
+    # TODO: Change this to use get_examination from ris_query_wrapper to make christoffer happy, yet still try to keep the querie files short
+    # rsp_name = rsp_path.split('.')[0]
+    # curr_exam = ris.get_examination(curr_rsp_dir, rsp_name)
+
+    ds = pydicom.dcmread(rsp_path)
+    rsp_info = (
+      ds.PatientID,
+      ds.PatientName,
+      ds.StudyDate,
+      ds.AccessionNumber,
+    )
+
+    print("Found patient with name: {0}".format(ds.PatientName))
+
+    # rsp_info = (
+    #   curr_exam.info['name'],
+    #   curr_exam.info['cpr'],
+    #   curr_exam.info['date'],
+    #   curr_exam.info['ris_nr'],
+    # )
+
+    rsps.append(rsp_info)
+
+  # Remove the search query file
+  os.remove(curr_search_file)
+  shutil.rmtree(curr_rsp_dir)
+
+  # Add specific bootstrap class to the form item
+  get_study_form = forms.GetStudy(initial=auto_fill_params)
+  for item in get_study_form:
+    item.field.widget.attrs['class'] = 'form-control'
 
   context = {
-    'getstudy' : forms.GetStudy(),
-    'responses': rsps
+    'getstudy' : get_study_form,
+    'responses': rsps,
+    
   }
 
   return HttpResponse(template.render(context, request))
