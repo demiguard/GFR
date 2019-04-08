07848fe7e99c40bce37485f0d8ff47a89c5cb693
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-04-08 10:00:59 +0200

    Made ajax_login and logout class-based

--- main_page/urls.py
+++ main_page/urls.py
@@ -9,16 +9,16 @@ Startup.start_up()
 app_name = 'main_page'
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
-  path('list_studies/new_study', views.NewStudy.as_view(), name='new_study'),
-  path('list_studies', views.ListStudies.as_view(), name='list_studies'),
+  path('list_studies/new_study', views.NewStudyView.as_view(), name='new_study'),
+  path('list_studies', views.ListStudiesView.as_view(), name='list_studies'),
   path('fill_study/<str:rigs_nr>', views.fill_study, name='fill_study'),
   path('search', views.search, name='search'),
   path('present_study/<str:rigs_nr>', views.present_study, name='present_study'),
    path('present_old_study/<str:rigs_nr>', views.present_old_study, name='present_old_study'),
-  path('logout', views.logout_page, name='logout'),
+  path('logout', views.LogoutView.as_view(), name='logout'),
   path('settings', views.settings, name='settings'),
   path('documentation', views.documentation, name='documentation'),
   # Async ajax urls
-  path('ajax/login', views.ajax_login, name='ajax_login'),
+  path('ajax/login', views.AjaxLogin.as_view(), name='ajax_login'),
   path('ajax/search', views.ajax_search, name='ajax_search'),
 ]
