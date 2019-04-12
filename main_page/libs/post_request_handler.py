4269e826a5cf34b87973a4bd8af3b011cf920bc8
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2019-04-12 11:52:16 +0200

    deja vu, I've just been to this place before, Higher on the street, And i know it's my time to go...

--- main_page/libs/post_request_handler.py
+++ main_page/libs/post_request_handler.py
@@ -128,7 +128,7 @@ def fill_study_post(request, rigs_nr):
       history_clr_n=history_clrN,
       save_fig=False
     )
-    
+
     base_resp_dir = server_config.FIND_RESPONS_DIR
     hospital     = request.user.hospital
     img_path     = 'main_page/static/main_page/images'
@@ -207,7 +207,6 @@ def store_form(request, rigs_nr):
   base_resp_dir = server_config.FIND_RESPONS_DIR
   hospital = request.user.hospital
 
-
   if not os.path.exists(base_resp_dir):
     os.mkdir(base_resp_dir)
 
@@ -218,30 +217,29 @@ def store_form(request, rigs_nr):
 
   dicom_path = '{0}{1}.dcm'.format(DICOM_dirc, rigs_nr)  
 
-  #Initail Storing
-  dicomlib.store_dicom(dicom_path, update_dicom = True, update_date = True)
-  
-  dicomlib.store_dicom(dicom_path, series_number = rigs_nr[4:])
-  dicomlib.store_dicom(dicom_path, station_name = request.user.config.pacs_calling)
+  #All Fields to be stored
+  birthdate = None
+  injection_time = None
+  gfr_type = None
+  gender = None
+  injection_before = None
+  injection_after  = None
+  injection_weight = None
+  weight = None
+  height = None
+  bsa_method = 'Haycock'
+  seq = None
 
   # Store age
   if request.POST['birthdate']:    
-    clearance_math
-    dicomlib.store_dicom(
-      dicom_path,
-      age=request.POST['birthdate']
-    )
+    birthdate = request.POST['birthdate']
 
   #Injection Date Time information
   if len(request.POST['injection_date']) > 0:
     inj_time = request.POST['injection_time']
     inj_date = request.POST['injection_date']
     inj_datetime = date_parser.parse("{0} {1}".format(inj_date, inj_time))
-
-    dicomlib.store_dicom(
-      dicom_path,
-      injection_time = inj_datetime.strftime('%Y%m%d%H%M')
-    )
+    injection_time = inj_datetime.strftime('%Y%m%d%H%M')
 
   #Study Always exists
   study_type = int(request.POST['study_type'])
@@ -253,54 +251,28 @@ def store_form(request, rigs_nr):
   elif study_type == 2:
     study_str = 'Flere prøve Voksen'
 
-  #Store Study
-  dicomlib.store_dicom(
-    dicom_path,
-    gfr_type=study_str
-  )
+    gfr_type = study_str
 
   if len(request.POST['sex']) > 0:
-    dicomlib.store_dicom(
-      dicom_path,
-      gender=request.POST['sex']
-    )
+    gender = request.POST['sex']
 
   if (len(request.POST['vial_weight_before']) > 0) and (len(request.POST['vial_weight_after']) > 0):
-    vial_weight_before = float(request.POST['vial_weight_before'])
-    vial_weight_after = float(request.POST['vial_weight_after'])
-    vial_weight_inj   = vial_weight_before - vial_weight_after
-
-    dicomlib.store_dicom(
-      dicom_path,
-      injection_before = vial_weight_before,
-      injection_after  = vial_weight_after,
-      injection_weight = vial_weight_inj 
-      )
-
+    injection_before = float(request.POST['vial_weight_before'])
+    injection_after  = float(request.POST['vial_weight_after'])
+    injection_weight = injection_before - injection_after
   elif len(request.POST['vial_weight_before']) > 0:
-    vial_weight_before = float(request.POST['vial_weight_before'])
-    dicomlib.store_dicom(
-      dicom_path,
-      injection_before= vial_weight_before
-    )
-
-  bsa_method = 'Haycock'
+    injection_before = float(request.POST['vial_weight_before'])
+ 
   if (len(request.POST['weight']) > 0):
-    dicomlib.store_dicom(
-      dicom_path,
-      weight     = float(request.POST['weight']) 
-    ) 
+      weight = float(request.POST['weight']) 
+
   if (len(request.POST['height']) > 0):
-    dicomlib.store_dicom(
-      dicom_path,
-      height     = float(request.POST['height']),
-      bsa_method = bsa_method 
-    )
+      height = float(request.POST['height'])
 
   if request.POST['thin_fac']:
-    dicomlib.store_dicom(dicom_path, thiningfactor = float(request.POST['thin_fac']))
+    thiningfactor = float(request.POST['thin_fac'])
   if request.POST['std_cnt_text_box']:
-    dicomlib.store_dicom(dicom_path, std_cnt= float(request.POST['std_cnt_text_box']))
+    std_cnt= float(request.POST['std_cnt_text_box'])
 
   if int(request.POST['study_type']) == 2:
     sample_dates = request.POST.getlist('study_date')[:-1]
@@ -308,7 +280,6 @@ def store_form(request, rigs_nr):
   else:
     sample_dates = request.POST.getlist('study_date')
     sample_times = request.POST.getlist('study_time')
-    
 
   sample_tec99 = numpy.array([float(x) for x in request.POST.getlist('test_value')])
 
@@ -323,15 +294,27 @@ def store_form(request, rigs_nr):
     zip_obj_seq = zip(sample_datetimes, sample_tec99)
     seq = [(datetime, cnt) for datetime, cnt in zip_obj_seq]
     
-    dicomlib.store_dicom(
-      dicom_path,
-      sample_seq = seq
-    )
   else:
-    dicomlib.store_dicom(
-      dicom_path,
-      sample_seq=[]      
+      seq = []      
+
+  dicomlib.store_dicom(dicom_path, 
+    update_dicom = True,
+    update_date = True,
+    series_number = rigs_nr[4:],
+    station_name = request.user.config.pacs_calling,
+    gender=gender,
+    injection_before = injection_before,
+    injection_after  = injection_after,
+    injection_weight = injection_weight,
+    weight=weight,
+    height=height,
+    bsa_method=bsa_method,
+    thiningfactor=thiningfactor,
+    std_cnt=std_cnt,
+    sample_seq=seq
     )
+  
+
 
   logger.warning('Store_form Done')
 
