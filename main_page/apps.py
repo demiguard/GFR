3b17f3743122d0ca2c0d9f2cf5817257e3b67109
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2019-05-10 10:40:14 +0200

    asdf

--- main_page/apps.py
+++ main_page/apps.py
@@ -1,13 +1,14 @@
 from django.apps import AppConfig
 from .libs.query_wrappers import pacs_query_wrapper as pacs
+import Startup
 
 import logging
 
 class MainPageConfig(AppConfig):
     name = 'main_page'
     def ready(self):
-        init_logger()
-        init_dicom_env()
+        Startup.init_logger()
+        Startup.init_dicom_env()
         logger = logging.getLogger(name='ServerLogger')
         logger.info('Started Logger')
         try:
