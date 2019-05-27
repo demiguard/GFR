f377bfe41487a107ab7f570f1568201f460cef5f
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-05-27 09:02:55 +0200

    Added fetch service

--- rigs_fetcher/config.py
+++ rigs_fetcher/config.py
@@ -1,19 +1,19 @@
 import logging
 
-DB_FILEPATH = "/home/simon/Documents/clearance-stuff/GFR/db.sqlite3"
+DB_FILEPATH = "/home/gfr/GFR/db.sqlite3" # Filepath to sqlite database
 
-LOG_FILEPATH = "./log/fetcher.log"
-LOG_LEVEL = logging.INFO
+LOG_FILEPATH = "/home/gfr/GFR/rigs_fetcher/log/fetcher.log" # Filepath to log file
+LOG_LEVEL = logging.INFO  # Logging level
 
-INTERVAL = 20              # Interval to to execute function
+INTERVAL = 60 * 5              # Interval to to execute function
 INTERVAL_VARIANCE = 0     # Max random interval variance of 1 min.
 
 CALLING_AET = "RH_EDTA"           # Calling AET
 RIGS_AET = "VIMCM"             # Rigs AET
-RIGS_IP = "10.143.128.247"
-RIGS_PORT = 3320
-STORAGE_DIRECTORY = "/home/simon/Documents/clearance-stuff/GFR/active_dicom_objects/RH"
-ACCEPTED_PROCEDURES = (
+RIGS_IP = "10.143.128.247"  # Rigs server ip
+RIGS_PORT = 3320  # Port to rigs server
+STORAGE_DIRECTORY = "/home/gfr/GFR/active_dicom_objects/RH" # Where fetched dicom objects should be stored
+ACCEPTED_PROCEDURES = ( # Which procedures should be accepted by the fetcher
   'Clearance Fler-blodprøve', 
   'GFR, Cr-51-EDTA, one sampel',
   'GFR, Tc-99m-DTPA'
