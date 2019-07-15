d27071e97c5eb9d481ee3c3a65555d8ded1bbeab
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-07-15 14:54:21 +0200

    Added generic api endpoint for GET

--- main_page/views/api/hospital.py
+++ main_page/views/api/hospital.py
@@ -1,41 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import View
-from django.http import JsonResponse, HttpResponseNotFound
 
-from main_page import models
 from main_page.views.mixins import AdminRequiredMixin
-from main_page.views.api.serializers import JSONSerializer
+from main_page.views.api.generic_endpoints import GetEndpoint
+from main_page import models
 
 
-class GetEndpoint(View):
-  pass
+class HospitalEndpoint(AdminRequiredMixin, LoginRequiredMixin, GetEndpoint):
+  def __init__(self):
+    super().__init__(models.Hospital, fields={
+      'id',
+      'name',
+      'short_name',
+      'address'
+    })
 
-
-class HospitalEndpoint(AdminRequiredMixin, LoginRequiredMixin, View):
   def get(self, request, hospital_id=None):
-    context = { }
-    
-    serializer = JSONSerializer()
-    
-    if hospital_id:
-      try:
-        hospitals = [models.Hospital.objects.get(pk=hospital_id)]
-      except ObjectDoesNotExist:
-        return HttpResponseNotFound()
-    else:
-      hospitals = models.Hospital.objects.all()
-
-    # Serialize users
-    json_hospitals = [ ]
-    for hospital in hospitals:
-      json_hospitals.append(serializer.serialize(hospital, fields={
-        'id',
-        'name',
-        'short_name',
-        'address'
-      }))
-
-    context['users'] = json_hospitals
-
-    return JsonResponse(context)
+    return super().get(request, obj_id=hospital_id)
--- main_page/views/api/user.py
+++ main_page/views/api/user.py
@@ -1,37 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import View
-from django.http import JsonResponse, HttpResponseNotFound
 
-from main_page import models
+from main_page.views.api.generic_endpoints import GetEndpoint
 from main_page.views.mixins import AdminRequiredMixin
-from main_page.views.api.serializers import JSONSerializer
+from main_page import models
 
 
-class UserEndpoint(AdminRequiredMixin, LoginRequiredMixin, View):
+class UserEndpoint(AdminRequiredMixin, LoginRequiredMixin, GetEndpoint):
+  def __init__(self):
+    super().__init__(models.User, fields={
+      'id',
+      'username',
+      'department',
+      'user_group'      
+    })
+
   def get(self, request, user_id=None):
-    context = { }
-    
-    serializer = JSONSerializer()
-    
-    if user_id:
-      try:
-        users = [models.User.objects.get(pk=user_id)]
-      except ObjectDoesNotExist:
-        return HttpResponseNotFound()
-    else:
-      users = models.User.objects.all()
-
-    # Serialize users
-    json_users = [ ]
-    for user in users:
-      json_users.append(serializer.serialize(user, fields={
-        'id',
-        'username',
-        'department',
-        'user_group'
-      }))
-
-    context['users'] = json_users
-
-    return JsonResponse(context)
+    return super().get(request, obj_id=user_id)
