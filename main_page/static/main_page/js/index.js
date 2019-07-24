872da4d2ff764e12705a0ef8d31f878721eebaea
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-07-24 09:14:06 +0200

    Moved browser detection to the backend

--- main_page/static/main_page/js/index.js
+++ main_page/static/main_page/js/index.js
@@ -35,58 +35,9 @@ let enter_login = function(event) {
 }
 
 
-// Figures out the type and version number of the current userAgents browser
-get_browser = function() {
-  var ua= navigator.userAgent, tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [ ];
-  
-  if (/trident/i.test(M[1])){
-    tem = /\brv[ :]+(\d+)/g.exec(ua) || [ ];
-    return 'IE ' + (tem[1] || '');
-  }
-  
-  if (M[1] === 'Chrome'){
-    tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
-
-    if (tem!= null) {
-      return tem.slice(1).join(' ').replace('OPR', 'Opera');
-    }
-  }
-  
-  M = M[2] ? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-  
-  if ((tem = ua.match(/version\/(\d+)/i)) != null) {
-    M.splice(1, 1, tem[1]);
-  }
-  
-  return M.join(' ');
-}
-
-
 $(function() {
-  alerter.init_alerter($('#err-msg-container'));
-
   // Login events
   $('#login-btn').click(try_login);
   $('#id_password').keypress(enter_login);
   $('#id_username').keypress(enter_login);
-
-
-  // Detect browser type/version - warn user that they are using an unsupported browser
-  let browser = get_browser().toLowerCase();
-  var supported_browser = false;
-  let SUPPORTED_BROWSERS = [
-    'chrome',
-    'firefox'
-  ];
-
-  for (var i = 0; i < SUPPORTED_BROWSERS.length; i++) {
-    if (browser.includes(SUPPORTED_BROWSERS[i])) {
-      supported_browser = true;
-      break;
-    }    
-  }
-
-  if (!supported_browser) {
-    alerter.add_alert("Den anvendte browser er ikke understyttet af denne hjemmeside. Visse funktioner fungere muligvis ikke som ønsket. Det anbefales at man anvender en understøttet browser: Google Chrome eller Mozilla Firefox, som kan hentes i Software shoppen.", 'warning');
-  }
 });
--- main_page/views/index.py
+++ main_page/views/index.py
@@ -5,6 +5,30 @@ from main_page.libs import server_config
 from main_page import forms
 
 
+def is_browser_supported(user_agent: str) -> bool:
+  """
+  Determines if the browser is supported
+
+  Args:
+    user_agent: the user agent string
+
+  Returns:
+    True if the browser is supported, False otherwise
+  """
+  SUPPORTED_BROWSERS = [
+    #'chrome',
+    'firefox'
+  ]
+
+  user_agent = user_agent.lower()
+
+  for browser in SUPPORTED_BROWSERS:
+    if browser in user_agent:
+      return True
+  
+  return False
+
+
 class IndexView(TemplateView):
   """
   Index page - serves as the login page
@@ -12,10 +36,16 @@ class IndexView(TemplateView):
   template_name = 'main_page/index.html'
 
   def get(self, request):
+    user_agent = request.META['HTTP_USER_AGENT']
+    browser_support = is_browser_supported(user_agent)
+
+    login_form = forms.LoginForm()
+
     context = {
       'title'     : server_config.SERVER_NAME,
       'version'   : server_config.SERVER_VERSION,
-      'login_form': forms.LoginForm()
+      'browser_supported': browser_support,
+      'login_form': login_form,
     }
 
     return render(request, self.template_name, context)
