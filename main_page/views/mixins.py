5aea189595f8a25586ba57ee448fc209b9233cba
Simon S. Andersen <simon.surland.andersen@regionh.dk>
2019-09-04 09:44:43 +0200

    Wrote logging mixin

--- main_page/views/mixins.py
+++ main_page/views/mixins.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import PermissionDenied
 
+import logging
+
 
 # TODO: Add logging to check who attempted to access
 class AdminRequiredMixin:
@@ -13,3 +15,23 @@ class AdminRequiredMixin:
         return super().dispatch(request, *args, **kwargs)
     
     raise PermissionDenied
+
+
+class LoggingMixin:
+  # Logs current user name, ip address and site
+
+  def dispatch(self, request, *args, **kwargs):
+    logger = logging.getLogger()
+    log_msg = ""
+
+    if request.user:
+      log_msg += f"User: {request.user.username}"
+    else:
+      log_msg += "User: None"
+    
+    log_msg += f", IP address: {request.META.get('REMOTE_ADDR')}"
+    log_msg += f", PATH: {request.META.get('PATH_INFO')}"
+
+    logger.info(log_msg)
+
+    return super().dispatch(request, *args, **kwargs)
