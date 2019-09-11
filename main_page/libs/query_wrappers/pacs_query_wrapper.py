cdfb812130325206c80e39a8d9a37c32c0405d84
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2019-09-11 10:53:24 +0200

    Additional check to see determinevalid

--- main_page/libs/query_wrappers/pacs_query_wrapper.py
+++ main_page/libs/query_wrappers/pacs_query_wrapper.py
@@ -285,9 +285,11 @@ def start_scp_server():
     # Infomation Retrieved
     # Availble vars retrieved_dataset, retrieved_meta_info
     
+    logger.info(f'Dataset:\n {retrieved_dataset}')
+
     if 'AccessionNumber' in retrieved_dataset:
       dicomlib.update_tags(retrieved_dataset)
-      if 0x00230010 in retrieved_dataset
+      if 0x00230010 in retrieved_dataset and retrieved_dataset.Modality = 'OT':
         filename = f'{retrieved_dataset.AccessionNumber}.dcm'
         fullpath = server_config.SEARCH_DIR + filename
         dicomlib.save_dicom(fullpath, retrieved_dataset)
