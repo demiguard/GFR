6e63b1144df5fb975d2bbb2932d67d4bbccf443f
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2020-05-18 16:31:14 +0200

    Older files no longer causes crash

--- main_page/libs/query_wrappers/ris_query_wrapper.py
+++ main_page/libs/query_wrappers/ris_query_wrapper.py
@@ -108,6 +108,10 @@ def move_to_deleted(
     accession_number
   )
 
+  #There already exists a deleted file delete it then move the old over
+  if deleted_file.exists():
+    os.remove(deleted_file.as_posix())
+
   shutil.move(active_file.as_posix(), deleted_file.as_posix())
 
   return True
