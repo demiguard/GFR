db58ccc699a30d0f1a31036ab6d4f287f1e07424
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2020-06-08 19:24:06 +0200

    removed typo

--- main_page/libs/ris_thread.py
+++ main_page/libs/ris_thread.py
@@ -263,9 +263,6 @@ class RisFetcherThread(Thread):
       - function self.update_self handles this currently
 
     """
-    django.db.ConnectionHandler.
-
-
     ae_title_b   = ris_find_ae.ae_title
     department   = self.departments[ae_title_b]
     pacs_ae_find = self.pacs_ae_finds[ae_title_b]
