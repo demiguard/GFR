a87915432ece74b2e1e8d250e6bfbc0ec4fda8bc
Christoffer Vilstrup Jensen <demiguard@gmail.com>
2024-11-07 14:58:41 +0100

    Loggin info

--- main_page/libs/samba_handler.py
+++ main_page/libs/samba_handler.py
@@ -211,8 +211,9 @@ def smb_get_all_csv(hospital:str, model_server_config, timeout: int = 60):
   error_messages = []
 
   now = datetime.now()
-
+  logger.info(sample_dir)
   for path in sample_dir.glob("*"):
+
     if not path.is_file():
       continue
 
